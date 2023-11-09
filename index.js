2a997343e4848e090f11c644cd28b25840ed754d
juanKersul <juan.manuel.kersul@gmail.com>
2023-11-09 19:29:24 -0300

    part3

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const Person = require("./models/person");
+const Person = require('./models/person');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-morgan.token("body", (req, res) => {
-  return JSON.stringify(req.body);
-});
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
   }
-  next(error);
+  return next(error);
 };
-const unknownEndpoint = (request, response) => {
-  return response.status(404).send({ error: "unknown endpoint" });
-};
+const unknownEndpoint = (request, response) => response.status(404).send({ error: 'unknown endpoint' });
 
 app.use(express.json());
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 );
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    return response.json(persons);
-  });
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => response.json(persons));
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
         return response.json(person);
-      } else {
-        return response.status(404).end();
       }
+      return response.status(404).end();
     })
     .catch((error) => next(error));
 });
 
-app.get("/info", function (request, response) {
+app.get('/info', (request, response) => {
   Person.find({}).then((persons) => {
     const res = `<p>Phonebook has info for ${persons.length} people<p/> 
     <p>${new Date()}<p/>`;
@@ -55,28 +50,24 @@ app.get("/info", function (request, response) {
   });
 });
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      return response.status(204).end();
-    })
+    .then((result) => response.status(204).end())
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
   const person = new Person({
     name: request.body.name,
     number: request.body.number,
   });
   person
     .save()
-    .then((savedPerson) => {
-      return response.json(savedPerson);
-    })
+    .then((savedPerson) => response.json(savedPerson))
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body;
   Person.findByIdAndUpdate(
     request.params.id,
@@ -84,18 +75,16 @@ app.put("/api/persons/:id", (request, response, next) => {
     {
       new: true,
       runValidators: true,
-      context: "query",
-    }
+      context: 'query',
+    },
   )
-    .then((updatedPerson) => {
-      return response.json(updatedPerson);
-    })
+    .then((updatedPerson) => response.json(updatedPerson))
     .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
-
+/* eslint no-console: ["error", { allow: ["log"] }] */
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
